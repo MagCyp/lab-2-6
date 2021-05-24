a976784313603b933037c4f92b8e7e7fd409a608
MagCyp <79080354+MagCyp@users.noreply.github.com>
2021-05-24 10:34:59 +0300

    Initial commit

---
+++ assets/app.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {  Button,
+  Container,
+  Grid,
+  Divider,
+  Header,
+  Icon,
+  Image,
+  Item,
+  Label,
+  Menu,
+  Segment,
+  Step,
+  Table,
+  Site,
+  List,
+  Sidebar,
+  Visibility,
+  Search,
+} from 'semantic-ui-react';
+import 'semantic-ui-less/semantic.less'; // if you do this once in your entry point file, you don't have to do it again in other files.
+
+
+
+/*  ReactDOM.render(
+  <Segment width = '70%'>
+  1
+  1
+  </Segment>,
+  document.getElementById('appRoot'),
+);
+*/
+ReactDOM.render(
+  <Menu
+  style={{size:'',position:'top', margin: '5% auto 2',}}
+  >
+  <Image src='C:\Users\dimar\Desktop\lab\app\img\purple.png' size='small' wrapped />
+  <Container>
+
+    <Menu
+    style={{border:0}}
+    >
+
+    </Menu>
+  </Container>
+  </Menu>
+  ,document.getElementById(''),
+);
+ReactDOM.render(
+  <Container>
+  </Container>
+  ,document.getElementsById(''));
